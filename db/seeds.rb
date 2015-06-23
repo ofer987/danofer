e82364b0aaece7cff8d921f6aae0afe9837af090
Dan Jakob Ofer <ofer987@gmail.com>
2015-06-23 09:19:07 -0400

    Added a new presentation: Documentation

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,5 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Presentation.create!(name: 'How to be a HomeStar Runner with Vim', unique_name: 'keyboard', presented_at: '2015-04-30', filename: 'presentations/keyboard_vim.pdf', location: '49 Spadina Avenue, Suite 206, Toronto, Ontario')
+Presentation.create(name: 'How to be a HomeStar Runner with Vim', unique_name: 'keyboard', presented_at: '2015-04-30', filename: 'presentations/keyboard_vim.pdf', location: '49 Spadina Avenue, Suite 206, Toronto, Ontario')
+Presentation.create(name: 'Documentation', unique_name: 'documentation', presented_at: '2015-06-24', filename: 'presentations/documentation.pdf', location: '500 King Street West, 4th Floor, Toronto, Ontario')
