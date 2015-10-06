fe8cd4923568466a0d0c42eef667db417e08051e
Dan Jakob Ofer <ofer987@gmail.com>
2015-10-05 21:27:11 -0400

    Added the third presentation
    Learning by Inspecting Troubled Code

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,4 @@
 
 Presentation.create(name: 'How to be a HomeStar Runner with Vim', unique_name: 'keyboard', presented_at: '2015-04-30', filename: 'presentations/keyboard_vim.pdf', location: '49 Spadina Avenue, Suite 206, Toronto, Ontario')
 Presentation.create(name: 'Documentation', unique_name: 'documentation', presented_at: '2015-06-24', filename: 'presentations/documentation.pdf', location: '500 King Street West, 4th Floor, Toronto, Ontario')
+Presentation.create(name: 'Learning by Inspecting Troubled Code', unique_name: 'bad_ruby', presented_at: '2015-10-06', filename: 'presentations/bad_ruby.pdf', location: 'Bitmaker Labs, 220 King Street West, Suite 200, Toronto, Ontario')
