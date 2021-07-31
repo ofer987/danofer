ab136a1578390cce2d558bd95442d4e7cb77f582
Dan Jakob Ofer <dan@ofer.to>
2021-07-31 16:22:01 -0400

    Create form using prototype class model

--- src/index.js
+++ src/index.js
@@ -1,6 +1,34 @@
+'use strict';
 import "./index.scss";
 
-Chair = new function() {
-    this.name = "Ikea";
-    this.name = "far";
+const FORM_OPENED = "contact-me-opened";
+const FORM_CLOSED = "contact-me-closed";
+
+function ContactMe() {
+    this.form = document.getElementById("contact-me");
+    this.toggleFormButton = document.getElementById("toggle-form-button");
+
+    this.init();
 }
+
+ContactMe.prototype.init = function() {
+    var self = this;
+
+    this.toggleFormButton.addEventListener("click", function() {
+        if (self.form.className === FORM_CLOSED) {
+            self.openForm(self.form);
+        } else {
+            self.closeForm(self.form);
+        }
+    });
+}
+
+ContactMe.prototype.openForm = function(form) {
+    form.className = FORM_OPENED;
+}
+
+ContactMe.prototype.closeForm = function(form) {
+    form.className = FORM_CLOSED;
+}
+
+new ContactMe();
