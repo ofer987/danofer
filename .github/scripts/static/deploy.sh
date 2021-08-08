b29ff35b97eabdaf1101f4f766edbc9705f863ae
Dan Jakob Ofer <dan@ofer.to>
2021-08-08 10:02:08 -0400

    Bug fix in GitHub Actions

--- .github/scripts/static/deploy.sh
+++ .github/scripts/static/deploy.sh
@@ -6,6 +6,14 @@ set -ex;
 server_rsa="./server_rsa";
 echo ${STATIC_SERVER_PRIVATE_KEY} > ${server_rsa};
 
+# Remove its permissions
+chmod g-r "${server_rsa}";
+chmod g-w "${server_rsa}";
+chmod g-x "${server_rsa}";
+chmod o-r "${server_rsa}";
+chmod o-w "${server_rsa}";
+chmod o-x "${server_rsa}";
+
 # Deploy the static files to the remote server
 scp \
     -o StrictHostKeyChecking=no \
