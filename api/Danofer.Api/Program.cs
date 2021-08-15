d888d02dddc1d4f4f45cd2600fe921439fbd4ebb
Dan Jakob Ofer <dan@ofer.to>
2021-08-15 15:37:26 -0400

    Refactoring Program.cs

--- api/Danofer.Api/Program.cs
+++ api/Danofer.Api/Program.cs
@@ -1,11 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace Danofer.Api
 {
@@ -13,7 +7,9 @@ namespace Danofer.Api
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            CreateHostBuilder(args)
+                .Build()
+                .Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
