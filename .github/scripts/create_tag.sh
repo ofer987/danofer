74680b54bf1c985871586c186c6b180c69d8b201
Dan Jakob Ofer <dan@ofer.to>
2023-09-24 16:05:31 -0400

    cicd bug fixes

--- .github/scripts/create_tag.sh
+++ .github/scripts/create_tag.sh
@@ -1,13 +1,6 @@
 #!/usr/bin/env bash
 
-set -ex;
-
-script_directory="$(dirname ${BASH_SOURCE[0]})";
-
-# Create the VERSION variable
-source "${script_directory}/create_version.sh";
-
-TAG_NAME="${VERSION}";
+TAG_NAME="releases/${VERSION}";
 
 git tag "${TAG_NAME}";
 git push --tags "https://${GITHUB_TOKEN}@github.com/ofer987/danofer.git";
