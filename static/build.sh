88e4f7a6946c5446fe904653af68365aa34f3982
Dan Jakob Ofer <dan@ofer.to>
2023-10-07 13:45:28 -0400

    chore(static): replace static with danofer

--- static/build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# GOOGLE_TAG_ID should already be set
-# (maybe by the CI/CD system?)
-echo "{\"google_tag_id\": \"$GOOGLE_TAG_ID\"}" > local.json;
-
-mustache local.json templates/index.html > index.html;
-npx webpack --env mode=production;
--- static/build_and_watch.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-GOOGLE_TAG_ID=123;
-echo "{\"google_tag_id\": \"$GOOGLE_TAG_ID\"}" > local.json;
-
-mustache local.json templates/index.html > index.html;
-npx webpack --env mode=development;
