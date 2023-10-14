b042b89a09949a5a85dfd684c37e3208b02704d1
Dan Jakob Ofer <dan@ofer.to>
2023-10-14 07:10:22 -0400

    chore(workflows): install SMTP server

--- .github/scripts/api/build_danofer_image.sh
+++ .github/scripts/api/build_danofer_image.sh
@@ -2,18 +2,16 @@
 
 set -ex;
 
-server_user='root';
-
 # Build the Docker image
 ssh \
     -o StrictHostKeyChecking=no \
     -o UserKnownHostsFile=/dev/null \
     -i "../${SERVER_RSA_PATH}" \
-    "${server_user}@${OFER_TO_DOMAIN}" \
+    "${USERNAME}@${OFER_TO_DOMAIN}" \
     "docker build \
-        --file Danofer.run.Dockerfile \
-        --build-arg DOCKER_USERNAME=${DOCKER_USERNAME} \
-        --build-arg IMAGE_NAME=${IMAGE_NAME} \
-        --build-arg VERSION=${VERSION} \
-        --tag "${IMAGE_NAME}_run:latest" \
-        ./;";
+      --file Danofer.run.Dockerfile \
+      --build-arg DOCKER_USERNAME=${DOCKER_USERNAME} \
+      --build-arg IMAGE_NAME=${IMAGE_NAME} \
+      --build-arg VERSION=${VERSION} \
+      --tag "${IMAGE_NAME}_run:latest" \
+      ./;";
