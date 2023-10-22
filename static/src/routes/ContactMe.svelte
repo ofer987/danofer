aaea3a626690753bce046ec51f8e371ace254839
Dan Jakob Ofer <dan@ofer.to>
2023-10-22 09:52:48 -0400

    fix(contact-me): margin between form and alerts

--- static/src/routes/ContactMe.svelte
+++ static/src/routes/ContactMe.svelte
@@ -158,25 +158,25 @@
 							placeholder="What would you like to ask me"
 							bind:value={message}
 						/>
-						<!-- Only display if email address is invalid --->
-						<div
-							class="alert alert-warning alert-dismissible fade show"
-							role="alert"
-							class:invalid={isEmailAddressValid}
-						>
-							The email address should be in the format of name@example.com
-							<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close" />
-						</div>
-
-						<div class="alert alert-warning" role="alert" class:invalid={state != 'failure'}>
-							Failed to send an email. Try again later
-						</div>
-
-						<div class="alert alert-primary" role="alert" class:invalid={state != 'success'}>
-							Email sent
-						</div>
 					</fieldset>
 				</form>
+
+				<div
+					class="alert alert-warning alert-dismissible fade show"
+					class:invalid={isEmailAddressValid}
+					role="alert"
+				>
+					The email address should be in the format of name@example.com
+					<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close" />
+				</div>
+
+				<div class="alert alert-warning" role="alert" class:invalid={state != 'failure'}>
+					Failed to send an email. Try again later
+				</div>
+
+				<div class="alert alert-primary" role="alert" class:invalid={state != 'success'}>
+					Email sent
+				</div>
 			</div>
 
 			<div class="modal-footer">
