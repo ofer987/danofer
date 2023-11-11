d4101e460c1e5b052e4301a09cd2a85ed7a76b87
Dan Jakob Ofer <dan@ofer.to>
2023-11-11 15:17:21 -0500

    simplify code

--- .github/scripts/api/build_danofer_image.sh
+++ .github/scripts/api/build_danofer_image.sh
@@ -10,8 +10,5 @@ ssh \
   "${USERNAME}@${OFER_TO_DOMAIN}" \
   "docker build \
     --file api/Danofer.run.Dockerfile \
-    --build-arg DOCKER_USERNAME=${DOCKER_USERNAME} \
-    --build-arg IMAGE_NAME=${API_IMAGE_NAME} \
-    --build-arg VERSION=${VERSION} \
     --tag "${API_IMAGE_NAME}_run:latest" \
     ./;";
--- api/Danofer.build.Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM mcr.microsoft.com/dotnet/sdk:6.0	AS build
-WORKDIR /source
-
-COPY Danofer.sln .
-COPY Danofer.Api/ Danofer.Api/
-
-RUN dotnet restore
-RUN dotnet clean
-RUN dotnet build
